c1895bef3a7ae4ce4fd3fe13c880c8c385aa139f
CyberAxe3108 <crismaramaro31@gmail.com>
2026-07-31 14:27:59 -0400

    added start script to deploy backend

--- Routes/ApplicationRoutes.js
+++ Routes/ApplicationRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { Application, Interview, User } = require('../models')
 
-// GET /api/applications  (supports ?status=applied&userId=1)
+// GET /api/applications  
 router.get('/', async (req, res) => {
   try {
     const { status, userId } = req.query
--- db/index.js
+++ db/index.js
@@ -1,8 +1,15 @@
-const { Sequelize } = require('sequelize')
 require('dotenv').config()
+const { Sequelize } = require('sequelize')
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-})
+const db = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize('postgres://postgres:root@localhost:5432/JobApplication')
 
 module.exports = db
